803ad84a41503f09a31a754c4d5dfaa296010738
Luat Pham <luat@ravebizz.com>
2022-04-20 13:48:04 -0400

    set dictionary demo

--- 03OptionalsAndDictionary/MyPlayground.playground/Contents.swift
+++ 03OptionalsAndDictionary/MyPlayground.playground/Contents.swift
@@ -44,27 +44,68 @@ import UIKit
 // MARK: - Set: Unordered, no duplicates
 
 /// Indexing into a set
-
+let primes: Set<Int> = [2, 3, 5, 7, 7, 7]
+primes[primes.startIndex]
+let odd: Set<Int> = [1,3,5,7,9]
 /// Ordering of elements is random
     let mySet2 = Set([1,2,3,4,5])
     var mySet = Set([1,2,3,4,5])
 /// Comparing two sets to each other
 /// .union, .intersection, .subtract
+let primePlusOdds = primes.union(odd)
+let oddPrimes = primes.intersection(odd)
+let arr = Array(oddPrimes)
+let set = Set(arr)
 
 // MARK: - Dictionary: Unordered, Unique Keys, Value can be anything
 
 /// Subscripting into Dictionary always returns optional value
-
+var dictionary: [String: Int] = [:]
+dictionary["I"] = 1
+dictionary["V"] = 5
+dictionary["X"] = 10
 /// Looping with (key, value)
-
+for (key, value) in dictionary {
+    print(value)
+}
 /// Looping using a tuple
+for pair in dictionary {
+    print(pair.value)
+}
 
 /// Using dictionary as a counter
 
+var optionalNum: Int? = 10
+if var optionalNum = optionalNum {
+    optionalNum += 1
+}
+optionalNum
+
+let romanStr = "VIIIVXV"
+var counter = [Character: Int]()
+for char in romanStr {
+    if let num = counter[char] {
+        counter[char] = num + 1
+    } else {
+        counter[char] = 1
+    }
+}
+print(counter)
+let value = counter["#"]
+
 /// Reducing an array of string to a dictionary counter
+///
+
+let digits = [1,3,4,6]
+let sum = digits.reduce(into: 0) { acummulation, nextValue in
+    acummulation += nextValue
+}
+//let sum = digits.reduce(into: 0) { $0 += $1 }
+print(sum)
+
 let letters = ["a", "a", "b", "b", "c"]
 let reducedCounter = letters.reduce(into: [:]) { accum, letter in
     accum[letter, default: 0] += 1
 }
-
+print(reducedCounter)
 
