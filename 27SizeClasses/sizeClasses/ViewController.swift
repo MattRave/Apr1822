98c10b9e01a628df527e61e6963e93a7848d1123
Luat Pham <luat@ravebizz.com>
2022-05-23 15:44:37 -0400

    add size class demo

---
+++ 27SizeClasses/sizeClasses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sizeClasses
+//
+//  Created by Luat on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
